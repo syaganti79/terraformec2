f8c0a30d00df0ae9e1a76d5474c37d85714cefcc
syaganti79 <srinivas.yaganti@gmail.com>
2019-02-04 20:53:55 +0000

    variable file to call the variables

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable aws_reg {
+  description = "this is AWS region"
+  default = "eu-west-2"
+  type = "string"
+}
+
+variable stack {
+   description = "this terrafrom tag"
+   default = "terraform"
+}
+  
