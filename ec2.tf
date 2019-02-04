d471ab7e411729b726a82a3fd772bdd09fd6615e
syaganti79 <srinivas.yaganti@gmail.com>
2019-02-04 20:51:22 +0000

    updated the file to Linux

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-  region  = "eu-west-2"
+  region  = "${var.aws_reg}"
 }
 
 resource "aws_instance" "ec2"
  {
-   ami = "ami-0b0a60c0a2bd40612"
+   ami = "ami-027e6966"
    instance_type = "t2.micro"
    tags = {
-        Name = "Ubuntu_ec2_instance"
+        Name = "RHEL_ec2_instance"
          } 
   }
 
-output "ip" {
+/*output "ip" {
  value = "${aws_instance.ec2.public_ip}"
 
-      }
+      }*/
